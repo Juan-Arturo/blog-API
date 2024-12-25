507c8889687bc4563021579f87e61b35f897dbd5
Juan-Arturo <jagpjagp002@gmail.com>
2024-12-25 17:08:59 -0600

    #4 #5 seeding tables

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use \Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //vaciar la tabla
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // crear la misma clave para todoslos usuarios
+        $password = Hash::make('123456');
+
+        User::create(['name' => 'Administrador', 'email' => 'admin@example.com', 'password' => $password]);
+
+        //Generar 10 usuarios
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'password' => $password,
+            ]);
+        }
+    }
+}
