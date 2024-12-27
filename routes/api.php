cb3ea67e5b8ca3a11357f259439d01ee6e0ce4d5
Juan-Arturo <jagpjagp002@gmail.com>
2024-12-27 15:28:25 -0600

    #8 Controlador de Article Vinculacion implicita entre controlador y ruta

--- routes/api.php
+++ routes/api.php
@@ -3,35 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Article;
+use App\Http\Controllers\ArticleController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
 
-Route::get('/articles', function () {
-     return Article::all();
-    });
-    
-    Route::get('/articles/{id}', function ($id) {
-        return Article::find($id);
-    });
-    
-    Route::post('/articles', function (Request $request) {
-        return Article::create($request->all());
-    });
-    
-    Route::put('/articles/{id}', function ($id, Request $request) {
-        //return Article::findOrFail($id)->update($request->all());  retorna 1 si se crea correctamente
-        $article = Article::findOrFail($id);
-        $article->update($request->all());
-        return $article;
-    });
-    
-    Route::delete('/articles/{id}', function ($id) {
-        Article::find($id)->delete();
-        return 204;
-    });
-    
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/{article}', [ArticleController::class, 'show']);
+Route::post('/articles', [ArticleController::class, 'store']);
+Route::put('/articles/{article}', [ArticleController::class, 'update']);
+Route::delete('/articles/{article}', [ArticleController::class, 'destroy']);
     
     
